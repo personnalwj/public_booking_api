37207307c54a94908ed53399a045d1615e501af4
Willy NOUBISSIE <willynoubissie@MBP-de-Willy.lan>
2024-03-10 19:30:24 +0100

    remove authz

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {} from 'nest-keycloak-connect';
-import { AuthzModule } from './authz/authz.module';
-import { MikroOrmModule } from '@mikro-orm/nestjs';
+// import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { SpotsModule } from './resources/spots/spots.module';
 import { UsersModule } from './resources/users/users.module';
 import { BookingsModule } from './resources/bookings/bookings.module';
 import { TimeSlotsModule } from './resources/time-slots/time-slots.module';
-import mikroOrmConfig from './../mikro-orm.config';
+// import mikroOrmConfig from './../mikro-orm.config';
 
 @Module({
   imports: [
-    AuthzModule,
-    MikroOrmModule.forRoot(mikroOrmConfig),
+    // MikroOrmModule.forRoot(mikroOrmConfig),
     SpotsModule,
     UsersModule,
     BookingsModule,
