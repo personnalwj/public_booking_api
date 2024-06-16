8b8ed66b85c0d7fb41d8a9c1a610a17dd26a4399
Willy NOUBISSIE <willyjunior91@gmail.com>
2024-06-16 23:39:20 +0200

    remove prefix

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('back-api');
   app.enableCors({
-    origin: [
-      process.env.FRONTEND_DOMAIN,
-      process.env.SUPERTOKENS_WEBSITE_DOMAIN,
-      'http://public-booking.front.localhost',
-      'http://localhost:3003',
-    ],
+    origin: ['http://public-booking.front.localhost', 'http://localhost:3003'],
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
